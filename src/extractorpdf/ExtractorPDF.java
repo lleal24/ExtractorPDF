61c13320986f0b11b7ef26f45da84b8d728abe11
lleal24 <lorenaleal24@gmail.com>
2019-05-06 15:20:35 -0500

    Generacion de la vista que tiene un boton para seleccionar el archivo pdf, se agregan un campo textarea que muestra el contenido del archivo pdf, al lado derecho cinco campos tipo textField que mostraran los campos: autor, titulo, correo, año y editorial, se utilizo la libreria jPDFText para la extraccion de texto y el uso de expresiones regulares patrones, para la revision del texto que cumpla con las palabras definidas, la extraccion del texto se tomo por lineas ya que por palabras, se limita la forma de imprimir lo que se necesita y hay los caracteres especiales no se tienen en cuenta

---
+++ src/extractorpdf/ExtractorPDF.java
@@ -0,0 +1,12 @@
+
+import extractorpdf.vistaExtractor;
+
+
+public class ExtractorPDF {
+
+    public static void main(String[] args){
+        vistaExtractor vista = new vistaExtractor();
+        vista.setVisible(true);
+       
+}
+}
